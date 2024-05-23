4f3cbded821d95adc5222389e598ae7f09706c52
KITQAQ <kitchan20040215@gmail.com>
2024-05-23 23:58:48 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,12 +2,7 @@ import { defineConfig } from "./src/config/init-config.ts";
 
 export default defineConfig({
   feedUrls: [
-    "https://mikanani.me/RSS/Bangumi?bangumiId=3240&subgroupid=203",
-    "https://mikanani.me/RSS/Bangumi?bangumiId=3141&subgroupid=583",
-    "https://mikanani.me/RSS/Bangumi?bangumiId=3215&subgroupid=34",
-    "https://mikanani.me/RSS/Bangumi?bangumiId=1505&subgroupid=422",
-    "https://mikanani.me/RSS/Bangumi?bangumiId=3330&subgroupid=44",
-    "https://mikanani.me/RSS/Bangumi?bangumiId=3341&subgroupid=370",
+    
   ],
   feed_concurrency: 4,
   job_concurrency: 8,
