b4edec589e8b62d29f2ebc82061386e5b9cf2766
Author <actions@github.com>
2024-05-23 16:00:51 +0000

    Add feed URL: https://mikanani.me/RSS/Bangumi?bangumiId=3290&subgroupid=583

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "./src/config/init-config.ts";
 
 export default defineConfig({
   feedUrls: [
-    
+    "https://mikanani.me/RSS/Bangumi?bangumiId=3290&subgroupid=583",
   ],
   feed_concurrency: 4,
   job_concurrency: 8,
